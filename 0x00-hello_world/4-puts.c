2ce89d418567c10ead4ed655e0d9c717564cd092
CodesbyEkan <ekanvicky@gmail.com>
2022-09-08 16:43:29 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/*
+ * main - Entry Point
+ *
+ * Return: Always 0 (Sucessful)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
