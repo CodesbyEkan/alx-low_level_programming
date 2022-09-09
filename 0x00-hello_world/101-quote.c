391af02443b4d40dc0f5bb6a2f0db63d0be6acd9
CodesbyEkan <ekanvicky@gmail.com>
2022-09-08 21:41:16 -0700

    error output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints out an error
+ * Return: 1 Error found
+ */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
