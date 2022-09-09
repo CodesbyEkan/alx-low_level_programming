a9c34a220cc9012f621b7f53a5185790cb80f12e
CodesbyEkan <ekanvicky@gmail.com>
2022-09-08 17:55:36 -0700

    print text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /*
- * main - Print a text
- * Return: Always 
+ * main - Entry point
+ *
+ * Description: We use puts function to print out text
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
