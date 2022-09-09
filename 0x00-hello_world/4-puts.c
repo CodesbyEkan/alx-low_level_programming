9ff529ab4d59fd7d57b1bbd02f0110b89cf31ff9
CodesbyEkan <ekanvicky@gmail.com>
2022-09-08 17:45:18 -0700

    print text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /*
- * main - Entry Point
- *
- * Return: Always 0 (Sucessful)
+ * main - Print a text
+ * Return: Always 
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
