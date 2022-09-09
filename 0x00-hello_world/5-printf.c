05f20db84aadf315db864f74ed400aeba413e7ba
CodesbyEkan <ekanvicky@gmail.com>
2022-09-08 19:14:34 -0700

    Print out statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print out some text
+ * return: ALways 0
+ */
+
+int main(void)
+{
+	puts("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
