29639b9b1ddafc162fb7e17cbbf9ac20bc12d2ae
CodesbyEkan <ekanvicky@gmail.com>
2022-09-14 01:30:30 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Definition: Print _putchar in nested loop.
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char ci[] = "_putchar";
+	int i;
+
+	for (i = 0; ch[i] != "\0"; i++)
+	{
+		_putchar(ch[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
