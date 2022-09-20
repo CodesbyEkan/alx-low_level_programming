028ebf52711cb0ffe06c2f9526d9f159889366e7
CodesbyEkan <ekanvicky@gmail.com>
2022-09-20 06:17:57 -0700

    0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - Entry point
+ * @n: pointer to reset to 90
+ *
+ * Description: resets value to 98
+ *
+ * Return: 0 as usual
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
