33f62c10ca9fa035edd33fa0b66378c90d7d0b39
Ekan Vick <ekanvicky@gmail.com>
2023-11-06 07:33:16 -0800

    Dog.h headeer files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
+/*
+ * struct dog - struct with which dog informations are stored
+ * @name: name of the dog
+ * @age: The dog's age
+ * @owner: The owner of the dog
+ *
+ * Description: A struct called "dog" that stores the name,
+ * age and it's owner's name
+ */
+
 struct dog
 {
 	char *name;
