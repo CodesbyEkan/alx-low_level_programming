589ac080ee7b77aa7eb7483f7f97f82394621236
CodesbyEkan <ekanvicky@gmail.com>
2023-11-16 17:48:52 -0800

    Complete task files

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,15 @@
-section .data
-	msg: db "Hello, main", 0x0a
-	msglen equ $-msg
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
+	SECTION .text
+	extern printf
 	global main
-
 main:
-	; write Hellomain to screen
-	mov eax, 1 ; syscall for write
-	mov edi, 1
-	mov rsi, msg
-	mov edx, msglen
-	syscall
-	mov eax, 60 ; 60 is exit
-	xor edi, edi ; exit (0)
-	syscall
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	retl
